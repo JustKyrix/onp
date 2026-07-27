47026b62becb1c90e1962dbbbced87624f99b94d
JustKyrix <kyrixfx@outlook.de>
2026-07-27 18:20:29 +0200

    Internal Server error fix (hopefully)

--- server/web.py
+++ server/web.py
@@ -815,14 +815,20 @@ def tourney_page(channel):
     joined = any(p['osu_id'] == me_id for p in players) if me_id else False
     me = next((p for p in players if p['osu_id'] == me_id), None)
     my_match = None
+    my_opponent = None
     if me:
         matches = list_tourney_matches(channel)
         my_match = next((m for m in matches
                          if m['p1_id'] == me['id'] or m['p2_id'] == me['id']), None)
+        if my_match:
+            if my_match['p1_id'] == me['id']:
+                my_opponent = my_match['p2_name']
+            else:
+                my_opponent = my_match['p1_name']
     return render_template('tourney.html', channel=channel, tourney=t,
                            tmaps=list_tourney_maps(channel), players=players,
                            joined=joined, me_name=session.get('player_osu_name'),
-                           my_match=my_match)
+                           my_match=my_match, my_opponent=my_opponent)
 
 
 @app.route('/t/<channel>/login')
